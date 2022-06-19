f3ef9909198c887b1bf22ae8b395006e4a45d659
yogesh <39838149+yogesh-m-2@users.noreply.github.com>
2022-06-19 13:38:52 +0530

    Delete teler_http_intrusion_detection directory

--- teler_http_intrusion_detection/internal/runner/endpoints.go
+++
@@ -1,5 +0,0 @@
-package runner
-
-const (
-	Google204 = "http://clients3.google.com/generate_204"
-)
--- teler_http_intrusion_detection/pkg/errors/abort_windows.go
+++
@@ -1,8 +0,0 @@
-// +build windows
-
-package errors
-
-import "os"
-
-// Abort specifies the os.Exit function
-var Abort = os.Exit
